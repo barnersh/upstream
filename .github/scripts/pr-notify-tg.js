b7e57d12048a5342502ad59848371edc94790614
ElvisZ <ElvisZ@gmail.com>
2025-03-14 10:34:37 +0800

    Revert "update"
    This reverts commit c79968f689c55fda75a26c4eccb5227829fd5042.

--- .github/scripts/pr-notify-tg.js
+++ .github/scripts/pr-notify-tg.js
@@ -11,7 +11,6 @@
         tgToken: process.env.TG_BOT_TOKEN,
         tgDeployNotifyMessageThreadId: process.env.TG_DEPLOY_NOTIFY_MESSAGE_THREAD_ID,
         tgDeployNotifyChatId: process.env.TG_DEPLOY_NOTIFY_CHAT_ID,
-        ggEvent: process.env.GG_EVENT
     };
 
 
@@ -28,7 +27,6 @@
             text: `
 新的 Pull Request
 🔔 *${prData.repo}*
-🔔 *${prData.ggEvent.pull_request.title}*
 📌 [#${prData.number}: ${prData.title}](${prData.url})
 👤 ${prData.author}
 🔀 \`${prData.branch}\` → \`${prData.base}\`
