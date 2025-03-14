19b2142c3f8b15fb132524bc76b049a56783c022
ElvisZ <ElvisZ@gmail.com>
2025-03-14 14:57:43 +0800

    update

--- .github/scripts/create-notion-task.js
+++ .github/scripts/create-notion-task.js
@@ -29,7 +29,7 @@
                         ]
                     },
                     URL: {
-                        url: issueUrl
+                        url: `${data.issueUrl}`
                     }
                 }
             })
